3fdaf63821b401019989f357654ac19c704fa370
Muhie <msalhaimus@gmail.com>
2022-08-02 16:04:52 +0100

    Add project files.

---
+++ bubblesort/bubblesort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std; // this means that you do not need to start the line with std
+int main(int argc, char** argv)
+{
+    unsigned int n = 0;
+    cout << "Hello this is a test of the buble sort in c++! please enter the size of the unordered array: ";
+    cin >> n;
+    cout << "You have chosen for the array to be: " << n << "\n";
+    int* bubble_array = new int[n];
+    for (int i = 0; i < n; i++) {
+        cout << "Please enter the: " << i+1 << " item in the list ";
+        int x = 0;
+        cin >> x;
+        bubble_array[i] = x;
+    } //getting all user inputs! bubble sort starts here!
+    int temp = 0;
+    bool swapped = 1;
+    while (swapped != 0) {
+        swapped = 0;
+        for (int i = 0; i < n; i++) {
+            if (bubble_array[i]  < bubble_array[i + 1]) {
+                temp = bubble_array[i + 1];
+                bubble_array[i + 1] = bubble_array[i];
+                bubble_array[i] = temp;
+                swapped = 1;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << bubble_array[i] << ",";
+    }
+
+}
